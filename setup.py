732bc185e33a81fb565f6a9f07a8a2d7362a29a7
Prashant <74547535+Prashant-student@users.noreply.github.com>
2022-05-29 21:54:28 +0530

    setupChange

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = 'a library for recommending ml algorithms'
 LONG_DESCRIPTION = 'A package that helps you to recommend machine learning algorithms'
 
